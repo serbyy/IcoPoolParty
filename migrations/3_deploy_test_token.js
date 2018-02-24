8d650b2cfe22f604d44c211aa195e415dfd0eb80
Shane van Coller <svancoller@productiveedge.com>
2018-02-24 15:35:04 -0600

    Updated how sample sale and token are deployed

--- migrations/3_deploy_test_token.js
+++ migrations/3_deploy_test_token.js
@@ -1,9 +1,12 @@
 /* global artifacts */
 
-const CustomSaleArtifact  = artifacts.require("./test-contracts/CustomSale.sol");
-const GenericToken  = artifacts.require("./test-contracts/GenericToken.sol");
+const CustomSaleArtifact = artifacts.require("./test-contracts/CustomSale.sol");
+const GenericToken = artifacts.require("./test-contracts/GenericToken.sol");
 
-module.exports = function (deployer, network, accounts) {
-    deployer.deploy(CustomSaleArtifact, web3.toWei("0.05"));
-    deployer.deploy(GenericToken);
+module.exports = function (deployer, network) {
+    if (network == "develop" || network == "development") {
+        deployer.deploy(GenericToken).then(function () {
+            return deployer.deploy(CustomSaleArtifact, web3.toWei("0.05"), GenericToken.address);
+        });
+    }
 };
