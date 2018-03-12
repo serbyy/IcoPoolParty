e8b26cbd8c193fb7ea11f022b27070983b16d3f1
Shane van Coller <svancoller@productiveedge.com>
2018-03-12 11:14:49 -0500

    Updated config file to output js instead of JSON

--- test/utils.js
+++ test/utils.js
@@ -2,12 +2,12 @@ const fs = require("fs");
 const poolPartyFactoryConfig = JSON.parse(fs.readFileSync("../build/contracts/IcoPoolPartyFactory.json"));
 const poolPartyConfig = JSON.parse(fs.readFileSync("../build/contracts/IcoPoolParty.json"));
 
-let configArray = {"PoolPartyFactoryAbi": poolPartyFactoryConfig.abi, "PoolPartyAbi": poolPartyConfig.abi};
+let configObj = {"PoolPartyFactoryAbi": poolPartyFactoryConfig.abi, "PoolPartyAbi": poolPartyConfig.abi};
 
 const utils = {
     addKeyToDappConfig: async (_key, _value) => {
-        configArray[_key] = _value;
-        fs.writeFileSync("./build/contract-config.json", JSON.stringify(configArray));
+        configObj[_key] = _value;
+        fs.writeFileSync("./build/contract-config.js", "var contractConfig = " + JSON.stringify(configObj) + ";");
     }
 };
 
